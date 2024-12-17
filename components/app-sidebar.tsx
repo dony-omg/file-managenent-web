2fa0d2059e66749e8f5e86e70f4a15beb1453e4e
Minh Dao <minh.dao@kyanon.digital>
2024-12-17 15:39:07 +0700

    UI vehicle list page v1

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,79 +1,83 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { VersionSwitcher } from "@/components/version-switcher"
+import { VersionSwitcher } from '@/components/version-switcher';
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-    SidebarRail,
-} from "@/components/ui/sidebar"
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail
+} from '@/components/ui/sidebar';
 
 // This is sample data.
 const data = {
-    versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-    navMain: [
+  versions: ['1.0.1', '1.1.0-alpha', '2.0.0-beta1'],
+  navMain: [
+    {
+      title: 'Quản lý',
+      url: '#',
+      items: [
         {
-            title: "Quản lý",
-            url: "#",
-            items: [
-                {
-                    title: " Quản lý hồ sơ",
-                    url: "/admin/documents",
-                },
-            ],
+          title: ' Quản lý hồ sơ',
+          url: '/admin/documents'
         },
         {
-            title: "Thiết lập hệ thống",
-            url: "#",
-            items: [
-                {
-                    title: "Quản lý người dùng",
-                    url: "/admin/user",
-                },
-                {
-                    title: "Hoạt động",
-                    url: "/admin/activity-logging",
-                },
-            ],
+          title: 'Danh sách phương tiện',
+          url: '/admin/vehicle-list'
+        }
+      ]
+    },
+    {
+      title: 'Thiết lập hệ thống',
+      url: '#',
+      items: [
+        {
+          title: 'Quản lý người dùng',
+          url: '/admin/user'
         },
-    ],
-}
+        {
+          title: 'Hoạt động',
+          url: '/admin/activity-logging'
+        }
+      ]
+    }
+  ]
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-    return (
-        <Sidebar {...props}>
-            <SidebarHeader>
-                <VersionSwitcher
-                    versions={data.versions}
-                    defaultVersion={data.versions[0]}
-                />
-            </SidebarHeader>
-            <SidebarContent>
-                {/* We create a SidebarGroup for each parent. */}
-                {data.navMain.map((item) => (
-                    <SidebarGroup key={item.title}>
-                        <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                {item.items.map((item) => (
-                                    <SidebarMenuItem key={item.title}>
-                                        <SidebarMenuButton asChild>
-                                            <a href={item.url}>{item.title}</a>
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))}
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                    </SidebarGroup>
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <VersionSwitcher
+          versions={data.versions}
+          defaultVersion={data.versions[0]}
+        />
+      </SidebarHeader>
+      <SidebarContent>
+        {/* We create a SidebarGroup for each parent. */}
+        {data.navMain.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
                 ))}
-            </SidebarContent>
-            <SidebarRail />
-        </Sidebar>
-    )
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
