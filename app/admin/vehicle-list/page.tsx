5b0ccc35ae17babc82a08c5893435eb711384256
Minh Dao <minh.dao@kyanon.digital>
2024-12-20 17:04:11 +0700

    rebase

---
+++ app/admin/vehicle-list/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import TableVehicleList from './tableVehicle';
+
+const VehicleListPage = () => {
+  // fetch('/api/vehicles', {
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // }).then((data: any) => {
+  //   console.log(data);
+  // });
+
+  return <TableVehicleList data={''} />;
+};
+
+export default VehicleListPage;
